71a71962711557f9d23f9ba4e58a43df0d42f1ec
zxeni1 <zacxfitness@gmail.com>
2024-03-25 11:46:46 +1000

    Added workout and tables seeded

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -1,6 +1,10 @@
+from datetime import date
+
 from flask import Blueprint
+
 from init import db, bcrypt
 from models.user import User
+from models.workout import Workout
 
 db_commands = Blueprint('db', __name__)
 
@@ -30,6 +34,40 @@ def seed_tables():
     ]
 
     db.session.add_all(users)
+    db.session.commit()
+
+    workouts = [
+        Workout(
+            workout_name="Chest Day",
+            description="chest and triceps",
+            date=date.today(),
+            workout_rating="4/5",
+            user_id=users[0].id
+        ),
+        Workout(
+            workout_name="Leg Day",
+            description="Legs and cardio weight session",
+            date=date.today(),
+            workout_rating="5/5",
+            user_id=users[0].id
+        ),
+        Workout(
+            workout_name="Back and Biceps",
+            description="back and biceps weight session",
+            date=date.today(),
+            workout_rating="2/5",
+            user_id=users[1].id
+        ),
+        Workout(
+            workout_name="Cardio Session",
+            description="Bike and treadmill cardio session",
+            date=date.today(),
+            workout_rating="3/5",
+            user_id=users[1].id
+        )    
+    ]
+
+    db.session.add_all(workouts)
     db.session.commit()
 
     print("Tables seeded")
--- models/user.py
+++ models/user.py
@@ -10,13 +10,10 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    workouts = db.relationship('Workout', back_populates='user', cascade='all delete')
+    workouts = db.relationship('Workout', back_populates='user', cascade='all, delete')
 
 
 class UserSchema(ma.Schema):
-
-    workouts = fields.List(fields.Nested('WorkoutSchema', exclude=['user']))
-
     class Meta: 
         fields = ('id', 'name', 'email', 'password', 'is_admin')
 
